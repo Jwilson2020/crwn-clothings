bb152c22a120705b8311d2a3f2fb7c923fd9fb18
MightyOne2020 <jwilson.jimoh@gmail.com>
2021-01-15 21:07:36 -0600

    Updating ourapp with shop page collectionpreview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
-import './pages/homepage/homepage.styles.scss';
+
+import ShopPage from './pages/shop/shop.component.jsx';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
